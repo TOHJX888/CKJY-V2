d3b3b8f2ad83acc958fcb2434fd0604ffc90631f
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2023-11-26 19:41:56 +0800

    fixed the button

--- CKJY V2/ProgressTab/ProgressView.swift
+++ CKJY V2/ProgressTab/ProgressView.swift
@@ -15,57 +15,77 @@ struct ProgressView: View {
     let lastActionDateKey = "lastActionDate"
     
     var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .stroke( // 1
-                        Color.blue.opacity(0.5),
-                        lineWidth: 30
-                    )
-                Circle() // 2
-                    .trim(from: 0, to: CGFloat(ingredientManager.totalPoints) / CGFloat(ingredientManager.pointsGoal)) // 1
-                    .stroke(Color.blue,
-                            style: StrokeStyle(
-                                lineWidth: 30,
-                                lineCap: .round
-                            )
-                    )
-                    .rotationEffect(.degrees(-90))
-                    .animation(.easeOut, value: CGFloat(ingredientManager.totalPoints) / CGFloat(ingredientManager.pointsGoal))
-            }
+        NavigationStack {
+            VStack {
+                HStack {
+                    Text("   Current points: \(ingredientManager.totalPoints)")
+                        .font(.title2)
+                    Image(systemName: "leaf.fill")
+                    Spacer()
+                }
+                Spacer()
+                ZStack {
+                    Circle()
+                        .stroke( // 1
+                            Color.blue.opacity(0.5),
+                            lineWidth: 30
+                        )
+                    Circle() // 2
+                        .trim(from: 0, to: CGFloat(ingredientManager.totalPoints) / CGFloat(ingredientManager.pointsGoal)) // 1
+                        .stroke(Color.blue,
+                                style: StrokeStyle(
+                                    lineWidth: 30,
+                                    lineCap: .round
+                                )
+                        )
+                        .rotationEffect(.degrees(-90))
+                        .animation(.easeOut, value: CGFloat(ingredientManager.totalPoints) / CGFloat(ingredientManager.pointsGoal))
+                    
+                }
                 .frame(width: 200, height: 200)
-            HStack {
-                Stepper("\(ingredientManager.totalPoints)", value: $ingredientManager.totalPoints)
-                    .padding(20)
-                    .background(.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(30)
-                Stepper("\(ingredientManager.pointsGoal)", value: $ingredientManager.pointsGoal)
-                    .padding(20)
-                    .background(.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(30)
+                //            HStack {
+                //                Stepper("\(ingredientManager.totalPoints)", value: $ingredientManager.totalPoints)
+                //                    .padding(20)
+                //                    .background(.blue)
+                //                    .foregroundColor(.white)
+                //                    .cornerRadius(30)
+                //            Stepper("\(ingredientManager.pointsGoal)", value: $ingredientManager.pointsGoal)
+                //                    .padding(20)
+                //                    .background(.blue)
+                //                    .foregroundColor(.white)
+                //                    .cornerRadius(30)
+                Spacer()
+                HStack {
+                    Text("Goal: \(ingredientManager.pointsGoal)")
+                        .font(.title)
+                    Image(systemName: "leaf.fill")
+                }
+                
+                ////            }
+                .padding()
+                Spacer()
             }
-            .padding()
-        }
-        .onAppear {
-            performActionIfNeeded()
-        }
-        .sheet(isPresented: $showSummary, onDismiss: {
-            ingredientManager.pointsGoal = newPointsGoal
-        }) {
-            Form {
-                Text("You achieved \(Int(Double(ingredientManager.totalPoints) / Double(ingredientManager.pointsGoal) * 100))% of your goal! Congratulations!")
-                Button("inc") {
-                    ingredientManager.totalPoints += 1
+            .navigationTitle("My Progress View")
+            .onAppear {
+                performActionIfNeeded()
+            }
+            .sheet(isPresented: $showSummary, onDismiss: {
+                ingredientManager.pointsGoal = newPointsGoal
+            }) {
+                Form {
+                    Text("You achieved \(Int(Double(ingredientManager.totalPoints) / Double(ingredientManager.pointsGoal) * 100))% of your goal! Congratulations!Set your goal for the new week!")
+                    //                Button("inc") {
+                    //                    ingredientManager.totalPoints += 1
+                    //                }
+                    //                Button("dec") {
+                    //                    ingredientManager.totalPoints -= 1
+                    //                }
+                    Stepper("New Goal: \(newPointsGoal)", value: $newPointsGoal, step: 5)
                 }
-                Button("dec") {
-                    ingredientManager.totalPoints -= 1
-                }
-                Stepper("New Goal: \(newPointsGoal)", value: $newPointsGoal, step: 5)
             }
         }
     }
+        
     
     func performActionIfNeeded() {
         let calendar = Calendar.current
