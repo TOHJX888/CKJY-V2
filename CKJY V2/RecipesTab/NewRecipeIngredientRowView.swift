6d02b9c0061154fcc67673a53e14076417e38806
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2023-11-26 11:33:24 +0800

    added colour and points to preset ingredients in the search bar for recipeview

--- CKJY V2/RecipesTab/NewRecipeIngredientRowView.swift
+++ CKJY V2/RecipesTab/NewRecipeIngredientRowView.swift
@@ -25,7 +25,11 @@ struct NewRecipeIngredientRowView: View {
                 .font(.footnote)
                 .foregroundColor(.gray)
             }
+            Spacer()
+            Image(systemName: ingredient.points == -1 ? "hand.thumbsdown.circle.fill" : ingredient.points == 0 ? "minus.circle.fill" : "hand.thumbsup.circle.fill")
         }
+        
+        .foregroundColor(ingredient.points == -1 ? .red : ingredient.points == 0 ? Color(red: 0.95, green: 0.7, blue: 0) : .green)
         .onAppear {
             for item in ingredientManager.selectedIngredients {
                 if item == ingredient {
